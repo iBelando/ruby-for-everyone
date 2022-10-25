148bee25df01398fdf6fba6ad56d378ba9af31ba
iBelando <ignaciobelando@gmail.com>
2022-10-25 19:16:22 -0300

    :sparkles: 10. Arrays

---
+++ arrays.rb
@@ -0,0 +1,15 @@
+system "clear"
+
+names = ["John", "Tim", "Mary", "Tina"]
+
+# puts names
+# puts names[0]
+# puts names.length
+puts names[names.length - 1]
+
+
+# Join two different arrays
+arr_1 = [1, 2, 3]
+arr_2 = [4, 5, 6, arr_1]
+
+puts arr_2
