906c63f7647808d375b1e650cf1e5577defe8089
iBelando <ignaciobelando@gmail.com>
2022-10-25 19:23:40 -0300

    :sparkles: 13. Hashes

---
+++ hashes.rb
@@ -0,0 +1,12 @@
+system "clear"
+
+names = ["John", "Tim", "Mary"]
+
+favorite_pizza = {
+    "John" => "Pepperoni",
+    "Tim" => "Mushroom",
+    "Mary" => "Cheese"
+}
+
+# puts favorite_pizza
+puts favorite_pizza["John"]
